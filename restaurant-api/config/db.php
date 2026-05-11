c804b2150e1b90f235ee09b363e3c8ebc70b04d8
jagath05 <jagathrachagan285@gmail.com>
2026-05-11 17:31:43 +0530

    railway

--- restaurant-api/config/db.php
+++ restaurant-api/config/db.php
@@ -1,19 +1,21 @@
 <?php
 
-$conn =
-mysqli_connect(
-"sql200.infinityfree.com",
-"if0_41882958",
-"Admin00o123",
-"if0_41882958_restaurant_db"
+$host = "viaduct.proxy.rlwy.net";
+$user = "root";
+$password = "CaHzIoPGwTpzoAsPNJyMQKIbbMrUsqNi";
+$database = "railway";
+$port = 36087;
+
+$conn = mysqli_connect(
+    $host,
+    $user,
+    $password,
+    $database,
+    $port
 );
 
-if(!$conn){
-
-die(
-"Database Connection Failed"
-);
-
+if (!$conn) {
+    die("Database Connection Failed: " . mysqli_connect_error());
 }
 
 ?>
